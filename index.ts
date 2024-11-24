7bd66ebec8fd3be9e26678d1f8e271fc7181b734
Dean McNeil <deanolmcneil@gmail.com>
2024-11-23 23:52:13 -0500

    Logging improvement

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import { shuffle } from "lodash";
 import { CANNOT_MATCH_LISTS } from "./cannotMatch";
 import { PARTICIPANTS, FAMILY_NAME, PARTICIPANT_NOTIFICATION_EMAILS } from "./participants";
 
-const IS_DEBUGGING = true;
+const IS_DEBUGGING = false;
 
-const print = (str: string) => {
-  if (IS_DEBUGGING) console.log(str);
+let proceedings = "";
+const appendToProceedings = (str: string) => {
+  if (proceedings) proceedings += "\n";
+  proceedings += str;
 };
 
 const numberParticipants = PARTICIPANTS.length;
@@ -32,7 +34,7 @@ const drawNames = () => {
     const giver = givers[matches];
     const receiver = receivers[matches];
     if (giver === receiver) {
-      print(`***${giver} drew themself, which is not allowed.`);
+      appendToProceedings(`***${giver} drew themself, which is not allowed.`);
       rotateToBack(matches);
       unsuccessfulDraws++;
       continue;
@@ -40,7 +42,7 @@ const drawNames = () => {
     try {
       for (const list of CANNOT_MATCH_LISTS)
         if (list.includes(giver) && list.includes(receiver)) {
-          print(`***${giver} drew ${receiver}, which is not allowed.`);
+          appendToProceedings(`***${giver} drew ${receiver}, which is not allowed.`);
           rotateToBack(matches);
           unsuccessfulDraws++;
           throw new Error();
@@ -48,7 +50,7 @@ const drawNames = () => {
     } catch (error) {
       continue;
     }
-    print(`${giver} drew ${receiver}.`);
+    appendToProceedings(`${giver} drew ${receiver}.`);
     matches++;
     if (matches === numberParticipants) {
       wasDrawingSuccessful = true;
@@ -59,24 +61,30 @@ const drawNames = () => {
 
 while (!wasDrawingSuccessful) {
   drawingRound++;
-  print(`\n\n\n${FAMILY_NAME} Gift Exchange - Christmas ${new Date().getFullYear()}\n\nRound ${drawingRound} - Proceedings:\n`);
+  appendToProceedings(`\n\n\n${FAMILY_NAME} Gift Exchange - Christmas ${new Date().getFullYear()}\n\nRound ${drawingRound} - Proceedings:\n`);
   drawNames();
   if (!wasDrawingSuccessful) {
     if (drawingRound === unsuccessfulRoundsLimit) {
-      print("\nThe maximum number of invalid drawings and unsuccessful rounds was reached, so proceedings will be terminated. Please use less restrictive filters for matchmaking and try again.");
+      appendToProceedings(
+        "\nThe maximum number of invalid drawings and unsuccessful rounds was reached, so proceedings will be terminated. Please use less restrictive filters for matchmaking and try again."
+      );
       break;
     }
-    print("\nThe maximum number of invalid drawings was reached, so proceedings will be restarted.");
+    appendToProceedings("\nThe maximum number of invalid drawings was reached, so proceedings will be restarted.");
     ({ givers, receivers } = shuffleParticipants());
     continue;
   }
-  print(`\nRound ${drawingRound} - Results:\n`);
-  for (let i = 0; i < numberParticipants; i++) print(`${givers[i]} drew ${receivers[i]}.`);
-  print("\n\nMerry Christmas! See you next year!\n");
+  appendToProceedings(`\nRound ${drawingRound} - Results:\n`);
+  for (let i = 0; i < numberParticipants; i++) appendToProceedings(`${givers[i]} drew ${receivers[i]}.`);
+  appendToProceedings("\n\nMerry Christmas! See you next year!\n");
   break;
 }
 
-if (wasDrawingSuccessful) {
-  // TODO: Send emails to participants with their matches.
+if (IS_DEBUGGING || !wasDrawingSuccessful) console.log(proceedings);
+else {
+  console.log("\nDrawing successful!");
   // TODO: Log the results of the drawing to a file instead of the console.
+  console.log("results logged to file"); // TODO: improve this message
+  // TODO: Send emails to participants with their matches.
+  // TODO: test email sending, make sure I KNOW!! if an email wasn't sent out
 }
